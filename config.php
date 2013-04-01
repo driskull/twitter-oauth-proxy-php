dc327b76eaac31f883944820a854606bb5416fcf
Matt Driscoll <mdriscoll@esri.com>
2013-04-01 15:20:45 -0700

    no message

---
+++ config.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+ * @file
+ * A single location to store configuration.
+ */
+
+define('CONSUMER_KEY', '');
+define('CONSUMER_SECRET', '');
+define('OAUTH_CALLBACK', 'http://url.to.this.location/callback.php');
+define('OAUTH_COOKIE', 'my_twitter_app_oauth');
+define('OAUTH_COOKIE_DOMAIN', '.esri.com');
